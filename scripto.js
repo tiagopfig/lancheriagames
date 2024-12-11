e7aacde3670f4f8073db737710842a7be7a12034
tiagopfig <tiagopfig123@gmail.com>
2024-12-11 09:03:52 -0300

    Update scripto.js

--- scripto.js
+++ scripto.js
@@ -8,11 +8,11 @@ let total = 0;
 
         function finalizeOrder() {
             if (total > 0) {
-                alert("Seu pedido totaliza R$" + total + ". Obrigado por pedir!");
+                alert("Seu pedido totaliza R$" + total + ". Obrigado por pedir! realize o pagamento no seguinte pix: 3cf8f5a2-6713-490d-8070-fce1bd7b49c0");
             } else if (total === 0) {
                 alert("Você não fez nenhum pedido.");
             } else {
-                alert("Seu pedido totaliza R$" + total + ". Você aplicou um desconto!");
+                alert("Seu pedido totaliza R$" + total + ". Boa sorte no desafio, vai precisar");
             }
         }
         function msgdes() {
